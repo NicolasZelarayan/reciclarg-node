633d6d038addd9d66d9f7e0c2c51723ffbfe307b
Nicolas <nicolaszelarayan.ez@gmail.com>
2023-06-20 01:30:43 -0300

    fix modules npm

--- user.js
+++ user.js
@@ -8,5 +8,6 @@ router.post('/users', userController.createUser);
 router.get('/users/:id', userController.getUserById);
 router.put('/users/:id', userController.updateUser);
 router.delete('/users/:id', userController.deleteUser);
+router.get('/users/:username', userController.getUserByUsername);
 
 module.exports = router;
