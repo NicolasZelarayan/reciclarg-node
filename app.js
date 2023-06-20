2062a353ad96b03011ad7d8c283179c8a8b07bd2
Nicolas <nicolaszelarayan.ez@gmail.com>
2023-06-20 03:27:25 -0300

    modificacion CORS

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 // Middleware para configurar los encabezados CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
